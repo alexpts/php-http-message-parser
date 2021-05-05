1cabe154d1c92369896c21ce6a79a1fe5caa6b83
alexpts <alexpts@yandex.ru>
2021-05-05 22:11:27 +0300

    phpdoc

--- src/Message/HeadersValidator.php
+++ src/Message/HeadersValidator.php
@@ -10,6 +10,7 @@ class HeadersValidator
      * @param array $headers
      *
      * It is more strict validate than RFC-7230
+     * https://nginx.org/docs/http/ngx_http_core_module.html#ignore_invalid_headers
      */
     public function validate(array $headers): void
     {
@@ -32,7 +33,7 @@ class HeadersValidator
             }
         }
 
-        if (preg_match("/^[\x20-\x7E\x80-\xFF]+$/", $allValues) !== 1) {
+        if ($allValues && preg_match("/^[\x20-\x7E\x80-\xFF]+$/", $allValues) !== 1) {
             throw new InvalidArgumentException('The value is incorrect for one of the header.');
         }
     }
